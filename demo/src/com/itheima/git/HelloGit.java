762ce14fd12884adacaa90d5e05cd9bc50276e7c
pllthxh <963061054@qq.com>
2017-08-14 17:12:12 +0800

    修改了一行代码

--- demo/src/com/itheima/git/HelloGit.java
+++ demo/src/com/itheima/git/HelloGit.java
@@ -3,6 +3,6 @@ package com.itheima.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("迷人的大姐姐,\n漂亮的小姐姐");
+		System.out.println("迷人的大姐姐,\n漂亮的小姐姐,\n可爱的小妹妹");
 	}
 }
